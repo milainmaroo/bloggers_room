f20c4d69441241db5f1ea7defe2451d64cc9a5bd
Mi Lainmar Oo <milainmar13@gmail.com>
2024-01-05 16:10:08 +1100

    Add gitignore file

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,12 @@ const express = require('express')
 // express app
 const app = express()
 
+// routes
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the app' })
+})
+
 // listen for requests
 app.listen(4000, () => {
-  console.log('listening on port 4000')
+  console.log('listening on port 4000!!!')
 })
